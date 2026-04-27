60eb996a00f8a5eebf4b2f9da35ba226eab6618a
isha-atif-dev <isha.atif02@gmail.com>
2026-04-27 21:42:35 +0100

    preprocessed

--- src/preprocess.py
+++ src/preprocess.py
@@ -45,7 +45,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-X_train.to_csv("data/processed/X_train.csv",index=False)
-X_test.to_csv("data/processed/X_test.csv",index=False)
-y_train.to_csv("data/processed/y_train.csv",index=False)
-y_test.to_csv("data/processed/y_test.csv",index=False)
+X_train.to_csv("data/processed/X_train.csv", index=False)
+X_test.to_csv("data/processed/X_test.csv", index=False)
+y_train.to_csv("data/processed/y_train.csv", index=False)
+y_test.to_csv("data/processed/y_test.csv", index=False)
